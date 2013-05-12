32d85ecbb52dfb13fe6ce32c6c67b0b5a7f70057
Sean Redmond <github-smr@sneakemail.com>
2013-05-12 15:06:12 -0400

    Tests for Client#user and Client#users

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -25,4 +25,50 @@ describe Bibliovore::Client do
       end
     end
   end
+
+  describe "#user" do
+    context "with a good id" do
+      it "returns a User object" do
+        @client.conn.stub!(:get).and_return(mock Faraday::Response,
+          :body => USERID_RESPONSE
+        )
+        results = @client.user('unittestuser')
+        results.should be_an_instance_of Bibliovore::User
+      end
+    end
+
+    context "with a bad id" do
+      it "throws an error" do
+        @client.conn.stub!(:get).and_return(mock Faraday::Response,
+          :body => NOUSERID_RESPONSE
+        )
+        expect{@client.user('unittestuser')}.to raise_error(Bibliovore::ApiError)
+      end
+    end
+  end
+
+  describe "#users" do
+    context "with a good id" do
+      it "returns an Array of User objects" do
+        @client.conn.stub!(:get).and_return(mock Faraday::Response,
+          :body => USER_RESPONSE
+        )
+        results = @client.users('unittestuser')
+        results.should be_an_instance_of Array
+        results.first.should be_an_instance_of Bibliovore::User
+      end
+    end
+
+    context "with a bad id" do
+      it "returns an Array of User objects" do
+        @client.conn.stub!(:get).and_return(mock Faraday::Response,
+          :body => NOUSER_RESPONSE
+        )
+        results = @client.users('unittestuser')
+        results.should be_an_instance_of Array
+        results.first.should be_nil
+      end
+    end
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,43 @@ LIBRARY_RESPONSE = %q<{
 NOLIBRARY_RESPONSE = %q<{
     "error": {
         "message": "No library 'nyplsdjkflsdfd' found.",
-        "request": "/libraries/nyplsdjkflsdfd?api_key=few2vjhmdnhjhw7xnajj9wxt"
+        "request": "/libraries/nyplsdjkflsdfd?api_key=abcdefghijklmnopqrstuvwx"
+    }
+}>
+
+USER_RESPONSE = %q<{
+    "count": 1,
+    "limit": 1,
+    "page": 1,
+    "pages": 1,
+    "users": [
+        {
+            "id": "123456789",
+            "name": "unittestuser",
+            "profile_url": "http://any.bibliocommons.com/collection/show/123456789"
+        }
+    ]
+}>
+
+NOUSER_RESPONSE = %q<{
+    "count": 0,
+    "limit": 1,
+    "page": 1,
+    "pages": 1,
+    "users": []
+}>
+
+USERID_RESPONSE = %q<{
+    "user": {
+        "id": "123456789",
+        "name": "unittestuser",
+        "profile_url": "http://any.bibliocommons.com/collection/show/123456789"
+    }
+}>
+
+NOUSERID_RESPONSE = %q<{
+    "error": {
+        "message": "No user '123456789' found.",
+        "request": "/users/123456789?api_key=abcdefghijklmnopqrstuvwx"
     }
 }>
